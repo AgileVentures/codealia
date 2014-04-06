5458ccc847d7b127fa14965383d4450cf096906a
Bryan Yap <bcollie115@gmail.com>
2014-04-06 12:19:53 +0100

    copy&paste solution to meta-tags from WebsiteOne

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     output = render(:file => "layouts/#{layout}")
     self.output_buffer = ActionView::OutputBuffer.new(output)
   end
+
+  def shared_meta_keywords
+    'Codealia, Coding for Girls, Women in Technology, Coding for Kids'
+  end
+
+  def default_meta_description
+    @default_meta_description ||= '' +
+    'An Open Source Solution For Technology Education. Codealia seeks to level the playing field by ' +
+    'developing a free open-source learning platform with an engaging curriculum targeting young girls. ' +
+    'Our interactive program will foster collaboration, teamwork and open communication which will pave ' +
+    'the way to future careers in the information technology field.'
+  end
 end
