245c30e26f32f124dd9ef95cb1e902bc189308fb
evo <CVMailer_2011@yahoo.com>
2014-04-18 18:19:37 -0700

    update posts_controller_spec.rb

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe PostsController do
   let(:post) {double :post}
+  before do
+      Post.stub(:find).and_return(post)
+    end
 
   describe 'GET index' do
       # it 'should i pass?' do
@@ -24,15 +27,37 @@ describe PostsController do
   end
 
   describe 'GET show' do
+    
       it 'does not call the authenticate method' do
         controller.should_not_receive(:authenticate)
-        get :show, {}
+        get :show, {id:'3'}
       end
 
       it 'does call the set_post method ' do 
-        controller.should_receive(:set_post)
-        get :show, {}
+        controller.should_receive(:set_post) # calls set_post but then does NOT RUN IT. Should_receive is STUB & ASSERTION
+        get :show, {id:'10'} #:id => '3'id: '3'
       end
   end
+
+
+  describe 'GET new' do
+      it 'does call the authenticate method' do
+        controller.should_receive(:authenticate)
+        get :new, {}
+      end
+      it 'does not call the set_post method ' do 
+        controller.should_not_receive(:set_post) # calls set_post but then does NOT RUN IT. Should_receive is STUB & ASSERTION
+        get :new, {} 
+      end
+      it 'assigns a new post to @post' do
+        Post.should_receive(:new).and_return('I like coffee')
+        get :new, {}
+        assigns(:post).should eq post
+      end
+
+
+
+  end
+
 
 end
