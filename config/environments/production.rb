4332d680a6563d3a9e4bc6fbd98b7d2989725c10
Bryan Yap <bcollie115@gmail.com>
2014-04-23 23:54:21 +0100

    add code-playground.css to the precompile list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Codealia::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( codealia-ide/ide.js )
+  config.assets.precompile += %w( codealia-ide/ide.js code-playground.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
