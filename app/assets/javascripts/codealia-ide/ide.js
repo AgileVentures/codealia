189cbdb6989a280a1d41589647d4060f1972f6d1
Bryan Yap <bcollie115@gmail.com>
2014-04-24 23:55:14 +0100

    add missing ace modules

--- app/assets/javascripts/codealia-ide/ide.js
+++ app/assets/javascripts/codealia-ide/ide.js
@@ -1,10 +1,10 @@
 //= require ace/ace
-//= require ace/mode-html
 //= require ace/worker-html
-//= require ace/mode-css
+//= require ace/mode-html
 //= require ace/worker-css
-//= require ace/mode-javascript
+//= require ace/mode-css
 //= require ace/worker-javascript
+//= require ace/mode-javascript
 //= require ace/theme-chrome
 //= require angular.min
 //= require angular-route.min
