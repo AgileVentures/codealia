2775a1849c4c2e3930be5460be7f794d46413153
Bryan Yap <bcollie115@gmail.com>
2014-04-27 12:45:31 +0100

    set enforce available locales to true

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Codealia
     # The default locale is :en and all translations from
     # config/locales/*.rb,yml and its sub-directories are auto loaded.
     config.i18n.default_locale = :en
+    config.i18n.enforce_available_locales = true
+    I18n.config.enforce_available_locales = true
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales',
                                                  '**', '*.{rb,yml}').to_s]
 
