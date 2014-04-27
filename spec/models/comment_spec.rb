6869c9df639184d179b1f044093cf55795c7a8cf
Bryan Yap <bcollie115@gmail.com>
2014-04-27 12:38:11 +0100

    making the dog happy

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,14 +5,15 @@ require 'spec_helper'
 
 describe Comment do
   describe 'validation' do
-    subject(:comment) {Comment.new}
-    before {comment.valid? }
+    subject(:comment) { Comment.new }
+    before { comment.valid? }
 
 
     [:post, :name, :body].each do |attribute|
       it "should validate presence of #{attribute}" do
         expect(comment).to have_at_least(1).error_on(attribute)
-        expect(comment.errors.messages[attribute]).to include I18n.t("activerecord.errors.messages.blank", attribute: attribute.capitalize)
+        expect(comment.errors.messages[attribute]).to include I18n.t(
+          'activerecord.errors.messages.blank', attribute: attribute.capitalize)
       end
     end
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 describe Post do
   describe 'validations' do
-    subject(:post) {Post.new}
+    subject(:post) { Post.new }
     before { post.valid? }
 
     [:title, :author, :content].each do |attribute|
       it "should validate presence of #{attribute}" do
         expect(post).to have_at_least(1).error_on(attribute)
-        expect(post.errors.messages[attribute]).to include I18n.t("activerecord.errors.messages.blank", attribute: attribute.capitalize)
+        expect(post.errors.messages[attribute]).to include I18n.t(
+          'activerecord.errors.messages.blank', attribute: attribute.capitalize)
       end
     end
   end
