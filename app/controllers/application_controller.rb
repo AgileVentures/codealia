2aa38580cb9049f754ca1987a28e9605fe2f024a
Bryan Yap <bcollie115@gmail.com>
2014-04-27 12:09:52 +0100

    making the dog happy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
 
-  def default_url_options(options={})
+  def default_url_options(options = {})
     { locale: I18n.locale }
   end
 
