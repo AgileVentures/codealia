307abd0aacb05d642e267185b5edf38d13fa6a6d
Bryan Yap <bcollie115@gmail.com>
2014-05-18 00:05:18 +0100

    refactored to using services instead of nested controllers, removed unnecessary controllers and directives

---
+++ app/assets/javascripts/directives/cdl_passions_editor.js
@@ -0,0 +1,11 @@
+CodealiaApp.directive("cdlPassionsEditor", [ "EditorsFactory",
+  function(EditorsFactory) {
+    return {
+      restrict: "A",
+      templateUrl: "/templates/passions-editor",
+      link: function(scope, element, attrs) {
+        EditorsFactory.setEditor(scope, element, attrs);
+      }
+    }
+  }
+]);
