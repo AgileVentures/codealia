65c692955fda478486c7b8c3dd570f50cd562dce
Pete Boucher <petebouch@gmail.com>
2014-05-23 21:25:59 +0200

    simplest solution, passing specs

--- spec/helpers/learning_helper_spec.rb
+++ spec/helpers/learning_helper_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 describe LearningHelper do
  describe 'LearningHelper#placeholder_img' do
-   it 'should return an img tag' do
-     expect(placeholder_img(100, 200)).to eq '<img src="http://placekitten.com/g/100/200">'
+   it 'should return an img tag with a src of the specified width' do
+     expect(placeholder_img(100)).to eq '<img src="http://placekitten.com/g/100/100">'
+   end
+
+   it 'should return an img tag with a src of the specified width and height' do
+     expect(placeholder_img(100, 150)).to eq '<img src="http://placekitten.com/g/100/150">'
    end
  end
 end
