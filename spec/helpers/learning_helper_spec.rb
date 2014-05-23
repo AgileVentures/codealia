8a654e6f82bb952d805e00058df2fbe3520273ba
Pete Boucher <petebouch@gmail.com>
2014-05-23 21:39:42 +0200

    fix html escaping

--- spec/helpers/learning_helper_spec.rb
+++ spec/helpers/learning_helper_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe LearningHelper do
  describe 'LearningHelper#placeholder_img' do
    it 'should return an img tag with a src of the specified width' do
-     expect(placeholder_img(100)).to eq '<img src="http://placekitten.com/g/100/100">'
+     expect(placeholder_img(100)).to eq '<img src="http://placekitten.com/g/100/100" />'
    end
 
    it 'should return an img tag with a src of the specified width and height' do
-     expect(placeholder_img(100, 150)).to eq '<img src="http://placekitten.com/g/100/150">'
+     expect(placeholder_img(100, 150)).to eq '<img src="http://placekitten.com/g/100/150" />'
    end
  end
 end
