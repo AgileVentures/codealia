7e4452efe819cc968e0ba6e386286e49434efd22
Pete Boucher <petebouch@gmail.com>
2014-05-26 17:17:17 +0200

    implement placeholder helper

--- app/helpers/learning_helper.rb
+++ app/helpers/learning_helper.rb
@@ -1,2 +1,7 @@
 module LearningHelper
+  def placeholder_img width, height
+    height = width unless height
+    url = "http://placekitten.com/g/#{width}/#{height}"
+    img_tag url, size: "#{width}x#{height}"
+  end
 end
