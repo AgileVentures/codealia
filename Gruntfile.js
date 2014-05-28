a123f0d5708e52fe2d90f18b58868df56dd3d4f4
johnnymo87 <johnnymo87@gmail.com>
2014-05-28 21:06:16 +0100

    copied from angularjs-seed-repo

---
+++ Gruntfile.js
@@ -0,0 +1,180 @@
+module.exports = function(grunt) {
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    shell: {
+      options: {
+        stdout: true
+      },
+      selenium: {
+        command: './selenium/start',
+        options: {
+          stdout: false,
+          async: true
+        }
+      },
+      protractor_install: {
+        command: 'node ./node_modules/protractor/bin/webdriver-manager update'
+      },
+      npm_install: {
+        command: 'npm install'
+      }
+    },
+
+    connect: {
+      options: {
+        base: 'app/'
+      },
+      webserver: {
+        options: {
+          port: 8888,
+          keepalive: true
+        }
+      },
+      devserver: {
+        options: {
+          port: 8888
+        }
+      },
+      testserver: {
+        options: {
+          port: 9999
+        }
+      },
+      coverage: {
+        options: {
+          base: 'coverage/',
+          port: 5555,
+          keepalive: true
+        }
+      }
+    },
+
+    protractor: {
+      options: {
+        keepAlive: true,
+        configFile: "./test/protractor.conf.js"
+      },
+      singlerun: {},
+      auto: {
+        keepAlive: true,
+        options: {
+          args: {
+            seleniumPort: 4444
+          }
+        }
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'app/scripts/{,*/}*.js'
+      ]
+    },
+
+    concat: {
+      styles: {
+        dest: './app/assets/app.css',
+        src: [
+          'app/styles/app.css',
+          //place your Stylesheet files here
+        ]
+      },
+      scripts: {
+        options: {
+          separator: ';'
+        },
+        dest: './app/assets/app.js',
+        src: [
+          'bower_components/angular/angular.js',
+          'bower_components/angular-route/angular-route.js',
+          'bower_components/angular-animate/angular-animate.js',
+          'app/scripts/homePages.js',
+          'app/scripts/app.js',
+          //place your JavaScript files here
+        ]
+      },
+    },
+
+    watch: {
+      options : {
+        livereload: 7777
+      },
+      assets: {
+        files: ['app/styles/**/*.css','app/scripts/**/*.js'],
+        tasks: ['concat']
+      },
+      protractor: {
+        files: ['app/scripts/**/*.js','test/e2e/**/*.js'],
+        tasks: ['protractor:auto']
+      }
+    },
+
+    open: {
+      devserver: {
+        path: 'http://localhost:8888'
+      },
+      coverage: {
+        path: 'http://localhost:5555'
+      }
+    },
+
+    karma: {
+      unit: {
+        configFile: './test/karma-unit.conf.js',
+        autoWatch: false,
+        singleRun: true
+      },
+      unit_auto: {
+        configFile: './test/karma-unit.conf.js',
+        autoWatch: true,
+        singleRun: false
+      },
+      unit_coverage: {
+        configFile: './test/karma-unit.conf.js',
+        autoWatch: false,
+        singleRun: true,
+        reporters: ['progress', 'coverage'],
+        preprocessors: {
+          'app/scripts/*.js': ['coverage']
+        },
+        coverageReporter: {
+          type : 'html',
+          dir : 'coverage/'
+        }
+      },
+    }
+  });
+
+  //single run tests
+  grunt.registerTask('test', ['jshint','test:unit', 'test:e2e']);
+  grunt.registerTask('test:unit', ['karma:unit']);
+  grunt.registerTask('test:e2e', ['connect:testserver','protractor:singlerun']);
+
+  //autotest and watch tests
+  grunt.registerTask('autotest', ['karma:unit_auto']);
+  grunt.registerTask('autotest:unit', ['karma:unit_auto']);
+  grunt.registerTask('autotest:e2e', ['connect:testserver','shell:selenium','watch:protractor']);
+
+  //coverage testing
+  grunt.registerTask('test:coverage', ['karma:unit_coverage']);
+  grunt.registerTask('coverage', ['karma:unit_coverage','open:coverage','connect:coverage']);
+
+  //installation-related
+  grunt.registerTask('install', ['update','shell:protractor_install']);
+  grunt.registerTask('update', ['shell:npm_install', 'concat']);
+
+  //defaults
+  grunt.registerTask('default', ['dev']);
+
+  //development
+  grunt.registerTask('dev', ['update', 'connect:devserver', 'open:devserver', 'watch:assets']);
+
+  //server daemon
+  grunt.registerTask('serve', ['connect:webserver']);
+};
